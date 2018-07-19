6e9dee10d526f9b941910ee7868c6e2fecb7c368
AdamSzklanko <adamszklanko+github@google.com>
2018-07-19 22:49:11 +0200

    Added parametrized tests.

--- src/test/java/com/example/demo/AnnotationAnalyzerTest.java
+++ src/test/java/com/example/demo/AnnotationAnalyzerTest.java
@@ -3,11 +3,14 @@ package com.example.demo;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.Map;
+import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayName("Annotation Analyzer Test")
 class AnnotationAnalyzerTest {
@@ -49,6 +52,21 @@ class AnnotationAnalyzerTest {
         }
     }
 
+    static Stream<Arguments> correctMappingForUser() {
+        return Stream.of(
+                Arguments.of("name", "USER_NAME"),
+                Arguments.of("fullName.surname", "SURNAME"),
+                Arguments.of("fullName.lastname", "LASTNAME"),
+                Arguments.of("fullName.size.id", "SIZE"),
+                Arguments.of("noname", "noname")
+        );
+    }
+
+    @Nested
+    @DisplayName("Negative")
+    class Negative {
+    }
+
     @Nested
     @DisplayName("PositiveJava8")
     class PositiveJava8 {
@@ -84,11 +102,24 @@ class AnnotationAnalyzerTest {
             //then
             assertTrue(parse.isEmpty());
         }
-    }
 
-    @Nested
-    @DisplayName("Negative")
-    class Negative {
-    }
+        @ParameterizedTest
+        @MethodSource("com.example.demo.AnnotationAnalyzerTest#correctMappingForUser")
+        void testWithMultiArgMethodSource(String key, String value) {
+            //when
+            Map<String, String> parse = AnnotationAnalyzer.parse(User.class);
 
+            //then
+            assertTrue(
+                    parse.containsKey(key),
+                    () -> String.format("The map doesn't contain the key: %s", key)
+            );
+            assertEquals(
+                    parse.get(key), value,
+                    () -> String.format("Should key: %s equal value: %s", key, value)
+            );
+        }
+
+
+    }
 }
